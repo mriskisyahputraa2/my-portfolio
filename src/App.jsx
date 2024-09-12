f80d7938ffac9f1213eb43318c8911ab5798e14b
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-09-12 22:54:49 +0700

    complete navbar page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
@@ -6,6 +7,9 @@ export default function App() {
       <div className="relative h-full overflow-y-hidden antialiased">
         <div className="fixed inset-0 bg-fixed bg-cover bg-center bg-img"></div>
         <div className="relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto">
+          <Navbar />
+
+          {/* hero section */}
           <Hero />
         </div>
       </div>
