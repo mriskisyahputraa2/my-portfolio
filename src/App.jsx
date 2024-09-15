956d02fb67aa24cdc3c1167e9a917cd1a614a4b2
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-09-15 20:58:16 +0700

    complete footer page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import ContactForm from "./components/ContactForm";
 import EducationSection from "./components/EducationSection";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
@@ -36,6 +37,9 @@ export default function App() {
 
           {/* Contact Form */}
           <ContactForm />
+
+          {/* Footer */}
+          <Footer />
         </div>
       </div>
     </>
